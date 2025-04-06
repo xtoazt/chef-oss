aaa591ddb9dee550f4871bb43f00824a9659815f
Sujay Jayakar <sujayakar@convex.dev>
2025-04-06 16:39:59 -0400

    .

---
+++ app/lib/stores/fileUpdateCounter.ts
@@ -0,0 +1,35 @@
+import { useStore } from "@nanostores/react";
+import { atom } from "nanostores";
+
+const fileUpdateCounter = atom(0);
+
+let currentTimer: NodeJS.Timeout | null = null;
+let lastUpdated = 0;
+const DEBOUNCE_TIME = 1000;
+
+export function useFileUpdateCounter() {
+    return useStore(fileUpdateCounter);
+}
+
+export function getFileUpdateCounter() {
+    return fileUpdateCounter.get();
+}
+
+export function incrementFileUpdateCounter() {
+    if (currentTimer) {
+        return;
+    }
+    const now = Date.now();
+    const nextUpdate = lastUpdated + DEBOUNCE_TIME;
+    if (now < nextUpdate) {
+        currentTimer = setTimeout(update, nextUpdate - now);
+        return;
+    }
+    update();
+}
+
+function update() {
+    fileUpdateCounter.set(fileUpdateCounter.get() + 1);
+    lastUpdated = Date.now();
+    currentTimer = null;
+}
