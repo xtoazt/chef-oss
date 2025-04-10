2a3bf2e6c3263df2fd530939896ebe24e31a7f3d
Ari <ari@convex.dev>
2025-04-10 12:04:08 -0700

    fix styling of suggestions (#262)
    <!-- Describe your PR here. -->
    
    
    
    <!--
      The following applies to third-party contributors.
      Convex employees and contractors can delete or ignore.
    -->
    
    ----
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

--- app/components/chat/SuggestionButtons.tsx
+++ app/components/chat/SuggestionButtons.tsx
@@ -32,13 +32,13 @@ export const SuggestionButtons = ({ chatStarted, onSuggestionClick, disabled }: 
   ];
 
   return (
-    <div id="suggestions" className="flex flex-col justify-center items-center">
-      <div className="flex gap-6">
+    <div id="suggestions">
+      <div className="flex gap-x-6 gap-y-4 flex-wrap justify-center">
         {suggestions.map((suggestion) => (
           <button
             key={suggestion.title}
             onClick={() => onSuggestionClick?.(suggestion.prompt)}
-            className="flex gap-1 items-center rounded-full px-3 py-1 border border-bolt-elements-borderColor bg-bolt-elements-item-backgroundDefault hover:bg-bolt-elements-item-backgroundActive text-bolt-elements-textSecondary hover:text-bolt-elements-textPrimary disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-bolt-elements-item-backgroundDefault disabled:hover:text-bolt-elements-textSecondary"
+            className="flex gap-1 min-w-fit items-center rounded-full px-3 py-1 border border-bolt-elements-borderColor bg-bolt-elements-item-backgroundDefault hover:bg-bolt-elements-item-backgroundActive text-bolt-elements-textSecondary hover:text-bolt-elements-textPrimary"
             disabled={disabled}
           >
             <ArrowUp className="size-4" />
