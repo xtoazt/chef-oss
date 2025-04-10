060f61a0567b011b09495949990f5f1ed361f9e2
Jordan Hunt <65152573+jordanhunt22@users.noreply.github.com>
2025-04-10 00:56:18 -0700

    Add splitwise prompt (#240)
    This is mostly copied over from the list of prompts except I did a bit
    of formatting. I tried this quite a few times and it worked every time.
    We should definitely have a few others test before landing, but this
    feels pretty good.

--- app/components/chat/SuggestionButtons.tsx
+++ app/components/chat/SuggestionButtons.tsx
@@ -15,8 +15,9 @@ export const SuggestionButtons = ({ chatStarted, onSuggestionClick, disabled }: 
 
   const suggestions = [
     {
-      title: 'Build a slack clone',
-      prompt: `Build an app similar to Slack. It should have a channels panel on the left with a button to create new channels. There should be a message pane on the right and a message posting box at the bottom. Each message should have a name and avatar next to it for the author of the message. There should be an "edit profile" tab that allows you to upload a profile photo and change your name. Make sure that the messages are the only thing that is scrollable. The message box and channel selector should stay fixed like the header. Make sure you scroll to the bottom when new messages are sent.`,
+      title: 'Build a Slack clone',
+      prompt:
+        'Build an app similar to Slack. It should have a channels panel on the left with a button to create new channels. There should be a message pane on the right and a message posting box at the bottom. Each message should have a name and avatar next to it for the author of the message. There should be an "edit profile" tab that allows you to upload a profile photo and change your name. Make sure that the messages are the only thing that is scrollable. The message box and channel selector should stay fixed like the header. Make sure you scroll to the bottom when new messages are sent.',
     },
     {
       title: 'Build an Instagram clone',
@@ -24,9 +25,9 @@ export const SuggestionButtons = ({ chatStarted, onSuggestionClick, disabled }: 
         'Build an app similar to Instagram except it\'s a global shared image stream with all users. There should be a box you can drag and drop images into to upload them. When uploading an image it should get resized to a maximum of 800x800 and be cropped to a square. There should be a "Stream" tab for viewing the global stream and a "My Photos" tab for viewing your own images. You should be able to delete your own photos in the "My Photos" tab. You should be able to click a button to like each image in the "Stream" tab and it should show the like count for each image.',
     },
     {
-      title: 'Build a todo app',
+      title: 'Build a Splitwise clone',
       prompt:
-        'Build a Todo app with two different tabs, "To Do" and "Done". Add the ability to add tags to a task when creating it and filter tasks by tag',
+        'Build a group shared expenses app that has the following features:\n\n• Has users, groups, expenses, payments, and reimbursements\n• Represents members in a group via a table rather than an array\n• Users can create groups and invite other users to join\n• Group members can add expenses to a group, which get shared among all members in the group\n• Shows a list of members in the group and a list of expenses along with who paid them\n• Show how much every member has been paid and reimbursed\n• Each member should be able to record a payment to another member, which adds to how much they have paid and adds to how much the recipient has been reimbursed\n• Members should record payments so that every member in the group has the same net balance',
     },
   ];
 
