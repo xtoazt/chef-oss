a7e9e9408108d21ea4ec67428775696fa249778d
Sarah Shader <sarah@convex.dev>
2025-04-22 15:39:48 -0400

    Fix debug command (#462)
    Was missing the leading `|` between issuer and subject

--- convex/dev.ts
+++ convex/dev.ts
@@ -43,7 +43,7 @@ export const findSessionForUser = internalQuery({
     const convexMember = await ctx.db
       .query("convexMembers")
       .withIndex("byTokenIdentifier", (q) =>
-        q.eq("tokenIdentifier", `https://auth.convex.dev/${normalizedGithubMemberId}`),
+        q.eq("tokenIdentifier", `https://auth.convex.dev/|${normalizedGithubMemberId}`),
       )
       .first();
     if (!convexMember) {
