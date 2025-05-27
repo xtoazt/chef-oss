0180bdaed200b70f29abb6269b7243c00ea0fb69
Emma Forman Ling <emma@convex.dev>
2025-05-27 14:30:58 -0700

    Remove override so we use recent convex versions in template (#667)

--- app/lib/stores/startup/useContainerSetup.ts
+++ app/lib/stores/startup/useContainerSetup.ts
@@ -23,7 +23,7 @@ import { chatSyncState } from '~/lib/stores/startup/history';
 import { FILE_EVENTS_DEBOUNCE_MS } from '~/lib/stores/files';
 import { setChefDebugProperty } from 'chef-agent/utils/chefDebug';
 
-const TEMPLATE_URL = '/template-snapshot-2a34ac7c.bin';
+const TEMPLATE_URL = '/template-snapshot-10f68703.bin';
 
 export function useNewChatContainerSetup() {
   const convex = useConvex();
