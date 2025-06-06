e284d68e3795f4269b643b474c042d76367785d6
Jordan Hunt <65152573+jordanhunt22@users.noreply.github.com>
2025-06-06 14:16:35 -0700

    Update Gemini to latest version (#704)
    https://blog.google/products/gemini/gemini-2-5-pro-latest-preview/

--- test-kitchen/initialGeneration.eval.ts
+++ test-kitchen/initialGeneration.eval.ts
@@ -70,9 +70,9 @@ if (process.env.OPENAI_API_KEY && process.env.USE_OPENAI === 'true') {
 
 if (process.env.GOOGLE_GENERATIVE_AI_API_KEY) {
   chefEval({
-    name: 'gemini-2.5-pro-preview-05-06',
-    model_slug: 'gemini-2.5-pro-preview-05-06',
-    ai: google('gemini-2.5-pro-preview-05-06'),
+    name: 'gemini-2.5-pro-preview-06-05',
+    model_slug: 'gemini-2.5-pro-preview-06-05',
+    ai: google('gemini-2.5-pro-preview-06-05'),
     maxTokens: 20000,
   });
 }
